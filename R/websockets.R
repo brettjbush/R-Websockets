f0729a308672f4c23da1cf98f9763044c85d029b
Brett Bush <brett.bush@thomsonreuters.com>
2016-10-31 15:08:41 -0500

    test

--- R/websockets.R
+++ R/websockets.R
@@ -43,16 +43,16 @@ websocket_write = function(DATA, WS)
     return(j)
   }
   if (WS$server$is.binary)
-    j = .SOCK_SEND(WS$socket,raw(2))
+    #j = .SOCK_SEND(WS$socket,raw(2))
   else
-    j = .SOCK_SEND(WS$socket,raw(1))
+    #j = .SOCK_SEND(WS$socket,raw(1))
   if(j<0) {
     websocket_close(WS)
     return(j)
   }
   cat("Made it!\n")
-  .SOCK_SEND(WS$socket,DATA)
-  .SOCK_SEND(WS$socket,packBits(intToBits(255))[1])
+  #.SOCK_SEND(WS$socket,DATA)
+  #.SOCK_SEND(WS$socket,packBits(intToBits(255))[1])
 }
 
 # "Broadcast" data to all the client websockets attached to the server.
