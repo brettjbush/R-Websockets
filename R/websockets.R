0cb30a3dfc2f2f26cecb0e905a67c0d05007cc31
Brett Bush <brett.bush@thomsonreuters.com>
2016-11-01 09:32:28 -0500

    test

--- R/websockets.R
+++ R/websockets.R
@@ -25,7 +25,7 @@ websocket_write = function(DATA, WS)
   }
   if(is.character(DATA)) DATA=charToRaw(DATA)
   if(!is.raw(DATA)) stop("DATA must be character or raw")
-  if(v==4){
+  if(v!=4){
     #j =.SOCK_SEND(WS$socket,.frame(length(DATA),mask=mask,
     #                                opcode=(if (WS$server$is.binary) { 2L } else { 1L })))
      frame = .frame(length(DATA),mask=mask, opcode=(if (WS$server$is.binary) { 2L } else { 1L }))
@@ -42,15 +42,19 @@ websocket_write = function(DATA, WS)
     if(j<0) websocket_close(WS)
     return(j)
   }
-  if (WS$server$is.binary)
+  if (WS$server$is.binary) {
+    cat("<A>")
     j = .SOCK_SEND(WS$socket,raw(2))
-  else
+  }
+  else {
+    cat("<B>")
     j = .SOCK_SEND(WS$socket,raw(1))
+  }
   if(j<0) {
     websocket_close(WS)
     return(j)
   }
-  cat("Made it!\n")
+  cat("<C>\n")
   .SOCK_SEND(WS$socket,DATA)
   .SOCK_SEND(WS$socket,packBits(intToBits(255))[1])
 }
